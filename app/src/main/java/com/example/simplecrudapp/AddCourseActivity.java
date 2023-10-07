17565cdf66ceeaf7f8fa1a00e5dabe50130245e0
Sundaee <justinthejasukmana@gmail.com>
2023-10-07 10:47:21 +0700

    finished AddCourseActivity.java and course_rv_item.xml

--- app/src/main/java/com/example/simplecrudapp/AddCourseActivity.java
+++ app/src/main/java/com/example/simplecrudapp/AddCourseActivity.java
@@ -1,14 +1,91 @@
 package com.example.simplecrudapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddCourseActivity extends AppCompatActivity{
+
+    // create variables
+    private TextInputEditText courseNameEdit, courseDescrEdit, coursePriceEdit,
+        courseSuitedEdit, courseImgEdit;
+    private ProgressBar progressBar;
+    private String courseID;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+
+        // initialize variables
+        courseNameEdit = findViewById(R.id.textEditCourseName);
+        courseDescrEdit = findViewById(R.id.textEditCourseDesc);
+        coursePriceEdit = findViewById(R.id.textEditCoursePrice);
+        courseSuitedEdit = findViewById(R.id.textEditCourseSuitedFor);
+        courseImgEdit = findViewById(R.id.textEditCourseImageLink);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // create our database reference
+        databaseReference = firebaseDatabase.getReference("Courses" );
+
+    }
+
+    public void onCourseAddBtn(View view){
+
+        // set progress bar visible
+        progressBar.setVisibility(View.VISIBLE);
+
+        // get datas
+        String courseName = courseNameEdit.getText().toString();
+        String courseDesc = courseDescrEdit.getText().toString();
+        String coursePrice = coursePriceEdit.getText().toString();
+        String courseSuited = courseSuitedEdit.getText().toString();
+        String courseImg = courseImgEdit.getText().toString();
+        courseID = courseName;
+
+
+
+        // pass data to modal class
+        CourseRVModal courseRVModal = new CourseRVModal(courseID, courseName, courseDesc, coursePrice, courseSuited, courseImg);
+
+        // call add value event to pass data into firebase db
+        databaseReference.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+
+                // set data in firebase db
+                databaseReference.child(courseID).setValue(courseRVModal);
+                // display toast message
+                Toast.makeText(AddCourseActivity.this, "Course added", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(AddCourseActivity.this, MainActivity.class));
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error){
+
+                // display error message
+                Toast.makeText(AddCourseActivity.this, "Failed to add course", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/simplecrudapp/CourseRVModal.java
+++ app/src/main/java/com/example/simplecrudapp/CourseRVModal.java
@@ -1,7 +1,122 @@
 package com.example.simplecrudapp;
 
-public class CourseRVModal{
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CourseRVModal implements Parcelable{
+
+    // create variables for fields
+    private String courseId;
+    private String courseName;
+    private String courseDescription;
+    private String coursePrice;
+    private String bestSuitedFor;
+    private String courseImg;
+
+    public CourseRVModal(String courseID, String courseName, String courseDesc, String coursePrice, String courseSuited, String courseImg){
+
+        this.courseId = courseID;
+        this.courseName = courseName;
+        this.courseDescription = courseDesc;
+        this.coursePrice = coursePrice;
+        this.bestSuitedFor = courseSuited;
+        this.courseImg = courseImg;
+
+    }
+
+    // setter getter
+    public String getCourseId(){
+        return courseId;
+    }
+
+    public void setCourseId(String courseId){
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public String getCoursePrice() {
+        return coursePrice;
+    }
+
+    public void setCoursePrice(String coursePrice) {
+        this.coursePrice = coursePrice;
+    }
+
+    public String getBestSuitedFor() {
+        return bestSuitedFor;
+    }
+
+    public void setBestSuitedFor(String bestSuitedFor) {
+        this.bestSuitedFor = bestSuitedFor;
+    }
+
+    public String getCourseImg() {
+        return courseImg;
+    }
+
+    public void setCourseImg(String courseImg) {
+        this.courseImg = courseImg;
+    }
+
+    // empty constructor
+    public CourseRVModal(){
 
 
+    }
+
+    protected CourseRVModal(Parcel parcel){
+
+        courseId = parcel.readString();
+        courseName = parcel.readString();
+        courseDescription = parcel.readString();
+        coursePrice = parcel.readString();
+        bestSuitedFor = parcel.readString();
+        courseImg = parcel.readString();
+
+    }
+
+    public static final Creator<CourseRVModal> CREATOR = new Creator<CourseRVModal>(){
+        @Override
+        public CourseRVModal createFromParcel(Parcel parcel){
+            return new CourseRVModal(parcel);
+        }
+
+        @Override
+        public CourseRVModal[] newArray(int i){
+            return new CourseRVModal[i];
+        }
+    };
+
+    @Override
+    public int describeContents(){
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags){
+
+        dest.writeString(courseId);
+        dest.writeString(courseName);
+        dest.writeString(courseDescription);
+        dest.writeString(coursePrice);
+        dest.writeString(bestSuitedFor);
+        dest.writeString(courseImg);
+
+    }
 
 }
